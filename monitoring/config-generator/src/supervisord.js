037e535ada1edea4e8d7044d6da0ee466ed2e0e2
M Yahya <ya7yaz@gmail.com>
2021-11-18 17:33:20 +0100

    initial loki integration, sans configs

--- monitoring/config-generator/src/supervisord.js
+++ monitoring/config-generator/src/supervisord.js
@@ -13,6 +13,10 @@ function generate (params, defaults = '/etc/supervisor.d/supervisord.conf') {
     obj['program:prometheus'].command = `/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=${ params.prometheusStoragePath || '/data/prometheus'} --storage.tsdb.retention.size=${params.prometheusRetentionSize || '0'} --web.route-prefix=${params.prometheusPrefix || '/'} --web.external-url=${params.prometheusExternalUrl || 'http://localhost:9090/prometheus'} --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles`
   }
 
+  if (params && params['loki-url']) {
+    obj['program:loki'].command = `/usr/bin/loki --config.file=/etc/loki/config.yaml`
+  }
+
   return ini.stringify(obj)
 }
 
