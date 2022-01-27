671feffcbd2b9729c7f5c9eb71c06993bd719157
Eli Mallon <eli@livepeer.org>
2022-01-27 10:07:27 -0800

    LP_SKIP_PROVISIONING only removes dashboards

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ "${LP_SKIP_PROVISIONING:-}" == "true" ]]; then
   echo "LP_SKIP_PROVISIONING is set, not overwriting dashboards"
-  rm -rf $GF_PATHS_PROVISIONING
+  rm -rf $GF_PATHS_PROVISIONING/dashboards/*
 fi
 node /config-generator/src/generate.js "$@"
 exec /usr/bin/supervisord -c /etc/supervisor.d/supervisord.conf -e debug
