a836e08c51d979eead92bcf758e38e4ad179ee82
Eli Mallon <eli@livepeer.org>
2022-01-27 09:52:23 -0800

    add LP_SKIP_PROVISIONING for prod

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+if [[ "${LP_SKIP_PROVISIONING:-}" == "true" ]]; then
+  echo "LP_SKIP_PROVISIONING is set, not overwriting dashboards"
+  rm -rf $GF_PATHS_PROVISIONING
+fi
 node /config-generator/src/generate.js "$@"
 exec /usr/bin/supervisord -c /etc/supervisor.d/supervisord.conf -e debug
